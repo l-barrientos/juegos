7f786b4645c37bf1574c450b22bb25d2b102c6ed
Luis <luisjin9@gmail.com>
2022-01-17 02:17:10 +0100

    all deleted

--- database.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Generate the users array with data from users.json file, if file doesn't exist it will be an empty array
-$users_array = json_decode(file_get_contents('users.json'), true);
