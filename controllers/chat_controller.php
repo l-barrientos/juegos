722523186937be9264163bc964ff7645d50786af
Luis <luisjin9@gmail.com>
2022-01-20 14:22:21 +0100

    trying to update chat

--- controllers/chat_controller.php
+++ controllers/chat_controller.php
@@ -6,7 +6,7 @@ $conn = new Connection();
 $messages_array = $conn->getMessages();
 
 
-if (isset($_POST['submit_message'])) {
+if (isset($_POST['submit_message']) &&  !empty($_POST['message_text']) && !ctype_space($_POST['message_text'])) {
     $conn->insertMessage($_COOKIE['user'], $_POST['message_text']);
     header('location: ./chat_controller.php');
 }
--- views/chat_view.php
+++ views/chat_view.php
@@ -16,19 +16,8 @@
     <div style='margin-left:10%;margin-right:10%;'>
         <h2>Chat</h2>
         <div id='chat' style="overflow-y: scroll; height:400px;">
-            <table class="table">
-                <?php
-                foreach ($messages_array as $msg) {
-                    $date = date('h:i', strtotime($msg['date']));
+            <table class="table" id="messages">
 
-                ?>
-                    <tr>
-                        <td style='width:7%;'> <?= "<img style='width:75%;height:6%;border-radius:100%;' src='" . '../' . $msg['profile_image'] . "' />"; ?></td>
-                        <td><b><?= $msg['user_name'] ?></b></td>
-                        <td><?= $msg['message'] ?></td>
-                        <td><?= $date ?></td>
-                    </tr>
-                <?php } ?>
             </table>
         </div>
         <form action="../controllers/chat_controller.php" method="post">
@@ -48,6 +37,49 @@
 <script>
     document.getElementById('chat').scrollTop = document.getElementById('chat').scrollHeight;
     document.getElementById('message_text').focus();
+    setInterval(() => {
+        document.getElementById('messages').innerHTML = '';
+        showMessages(document.getElementById('messages'));
+    }, 5);
+
+    function showMessages(table) {
+        <?php
+        $messages_array = $conn->getMessages();
+        $i = 0;
+        foreach ($messages_array as $msg) {
+            $date = date('H:i', strtotime($msg['date']));
+
+        ?>
+
+            let row<?= $i ?> = document.createElement('tr');
+
+            let imgTd<?= $i ?> = document.createElement('td');
+            imgTd<?= $i ?>.style = "width:7%;";
+            let img<?= $i ?> = document.createElement('img');
+            img<?= $i ?>.src = "../<?= $msg['profile_image'] ?>";
+            img<?= $i ?>.style = "width:75%;height:6%;border-radius:100%;";
+            imgTd<?= $i ?>.appendChild(img<?= $i ?>);
+            row<?= $i ?>.appendChild(imgTd<?= $i ?>);
+
+            let tdName<?= $i ?> = document.createElement('td');
+            let tdNameText<?= $i ?> = document.createTextNode("<?= $msg['name'] ?>");
+            tdName<?= $i ?>.appendChild(tdNameText<?= $i ?>);
+            row<?= $i ?>.appendChild(tdName<?= $i ?>);
+
+            let tdMsg<?= $i ?> = document.createElement('td');
+            let tdMsgText<?= $i ?> = document.createTextNode("<?= $msg['message'] ?>");
+            tdMsg<?= $i ?>.appendChild(tdMsgText<?= $i ?>);
+            row<?= $i ?>.appendChild(tdMsg<?= $i ?>);
+
+            let tdDate<?= $i ?> = document.createElement('td');
+            let tdDateText<?= $i ?> = document.createTextNode("<?= $date ?>");
+            tdDate<?= $i ?>.appendChild(tdDateText<?= $i ?>);
+            row<?= $i ?>.appendChild(tdDate<?= $i ?>);
+
+            table.appendChild(row<?= $i ?>);
+        <?php $i++;
+        } ?>
+    }
 </script>
 
 </html>
